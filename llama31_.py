71da0a522b73ce8656e5fdec283a7721f059cdb2
shirinyamani <amani.shirin@ucalgary.ca>
2024-10-29 11:59:45 -0600

    RoPE added

--- llama31_.py
+++ llama31_.py
@@ -39,13 +39,59 @@ class ModelArgs:
         assert self.n_kv_heads <= self.n_heads
         assert self.n_heads % self.n_kv_heads == 0, f'Use NICE numbers for your heads!'
         assert self.dim % self.n_heads == 0, f'Make sure to use dividable num_embed and num_heads!'
+# ----------------------------------Rotatary Positional Embeding----------------------------------
+def apply_scaling(freqs: torch.Tensor):
+    # RoPE scaling (values obtained from grid search)
+    scale_factor = 8
+    low_freq_factor = 1
+    high_freq_factor = 4
+    old_context_len = 8192  # original llama3 length
+    low_freq_wavelen = old_context_len / low_freq_factor
+    high_freq_wavelen = old_context_len / high_freq_factor
+    new_freqs = []
+    for freq in freqs:
+        wavelen = 2 * math.pi / freq
+        if wavelen < high_freq_wavelen:
+            new_freqs.append(freq)
+        elif wavelen > low_freq_wavelen:
+            new_freqs.append(freq / scale_factor)
+        else:
+            assert low_freq_wavelen != high_freq_wavelen
+            smooth = (old_context_len / wavelen - low_freq_factor) / (high_freq_factor - low_freq_factor)
+            new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
+    return torch.tensor(new_freqs, dtype=freqs.dtype, device=freqs.device)
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0, use_scaled: bool = False):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device, dtype=torch.float32)
+    if use_scaled: #for long context len than what its trained on!
+        freqs = apply_scaling(freqs)
+    freqs = torch.outer(t, freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    freqs_cis_real = torch.stack([freqs_cis.real, freqs_cis.imag], dim=-1)
+    return freqs_cis_real
+
+def apply_rotary_emb(x, freqs_cis):
+    # shape gymnastics let's go
+    # x is (B, T, n_heads, head_dim), e.g. (4, 8, 32, 128)
+    # freqs_cis is (T, head_dim/2, 2), e.g. (8, 64, 2)
+    xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
+    # xshaped is (B, T, n_heads, head_dim/2, 2), e.g. (4, 8, 32, 64, 2)
+    freqs_cis = freqs_cis.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
+    # freqs_cis becomes (1, T, 1, head_dim/2, 2), e.g. (1, 8, 1, 64, 2)
+    x_out2 = torch.stack(
+        [
+            xshaped[..., 0] * freqs_cis[..., 0] - xshaped[..., 1] * freqs_cis[..., 1],
+            xshaped[..., 1] * freqs_cis[..., 0] + xshaped[..., 0] * freqs_cis[..., 1],
+        ],
+        -1,
+    )
+    # x_out2 at this point is (B, T, n_heads, head_dim/2, 2), e.g. (4, 8, 32, 64, 2)
+    x_out2 = x_out2.flatten(3)
+    # x_out2 is now (B, T, n_heads, head_dim), e.g. (4, 8, 32, 128)
+    return x_out2.type_as(x)
+
 # ----------------------------------KV CACHE--------------------------------------------
-
-# ----------------------------------Positional Embeding--------------------------------------------
-def apply_rotary_emb():
-    pass
-
-# ----------------------------------GQA-------------------------------------------------------------
 def repeat_kv():
     pass
 
@@ -91,7 +137,6 @@ class Attention(nn.Module):
         #TODO: Add KV Cache
         B, T, C = x.shape #(batch_size, seq_len, dim)
         xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
-        
         xq = xq.view(B, T, self.n_local_heads, self.head_dim) 
         xk = xk.view(B, T, self.n_kv_heads, self.head_dim)
         xv = xv.view(B, T, self.n_kv_heads, self.head_dim)
@@ -136,6 +181,7 @@ class FeedForward(nn.Module): #acts like a map k:v where k is the input text n v
         hidden_dim,
         multiple_of,
         ffn_dim_multiplier):
+        
         super().__init__()
         hidden_dim = int(2 * hidden_dim / 3) #ffn 2/3 network
         if ffn_dim_multiplier is not None:
@@ -155,6 +201,7 @@ class Block(nn.Module):
         self.dim = args.dim
         self.head_dim = args.dim // args.n_heads
         self.attention = Attention(args)
+        
         self.mlp = FeedForward(
             dim = args.dim,
             hidden_dim= 4 * args.hidden_dim,
@@ -169,8 +216,7 @@ class Block(nn.Module):
         return out
 # ----------------------------------Main TF---------------------------------------------------
 class Transformer(nn.Module):
-    def __init__(self, params: ModelArgs):
-        #TODO: ADD self.freqs_cis   
+    def __init__(self, params: ModelArgs): 
         super().__init__()
         self.params = params 
         self.vocab_size = params.vocab_size
@@ -180,7 +226,13 @@ class Transformer(nn.Module):
         self.layers = nn.ModuleList(list(Block(params) for _ in range(params.n_layers)))
         self.norm = RMSNorm(params.dim)
         self.lm_head = nn.Linear(params.dim, params.vocab_size, bias=False)
-
+        
+        self.freqs_cis = precompute_freqs_cis(
+            dim = params.dim // params.n_heads,
+            end= params.max_seq_len * 2,
+            theta= params.rope_theta,
+            use_scaled=params.use_scaled_rope)
+        
     def forward(): #logits + loss
         pass
     
